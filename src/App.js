d514bf3dfd5288b96c630499fad260fef88b9641
Alena <alena.senkiv@gmail.com>
2021-01-17 22:15:20 +0200

    step1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { v4 as uuidv4 } from 'uuid';
 import s from './App.module.css';
 
 class App extends Component {
@@ -7,8 +8,51 @@ class App extends Component {
     name: '',
   };
 
+  nameInputId = uuidv4();
+
+  handleChange = e => this.setState({ name: e.currentTarget.value });
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.addContact(this.state.name);
+  };
+
+  addContact = name => {
+    const contact = {
+      id: uuidv4(),
+      name,
+    };
+
+    this.setState(prevState => ({
+      contacts: [contact, ...prevState.contacts],
+    }));
+  };
+
   render() {
-    return <div className={s.container}>Phonebook</div>;
+    const { name, contacts } = this.state;
+    return (
+      <div className={s.container}>
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor={this.nameInputId}>
+            <input
+              type="text"
+              name="name"
+              id={this.nameInputId}
+              value={name}
+              onChange={this.handleChange}
+            ></input>
+          </label>
+          <button type="submit">Добавить контакт</button>
+        </form>
+        <h2>Contacts</h2>
+        <ul>
+          {contacts.map(({ name }) => (
+            <li key={uuidv4()}>{name}</li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 }
 
